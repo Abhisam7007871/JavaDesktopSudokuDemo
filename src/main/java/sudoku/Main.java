2e1a73a1e327961f84ae297baa6f923ffc6f59ae
Abhishek <abhisam7007871@gmail.com>
2024-02-24 00:23:36 +0530

    INITIAL COMMIT

---
+++ src/main/java/sudoku/Main.java
@@ -0,0 +1,9 @@
+package sudoku;
+
+import sudoku.problemdomain.SudokuGame;
+
+public class Main {
+    public static void main(String[] args) {
+        SudokuApplication.main(new String[]{});
+    }
+}
--- src/main/java/sudoku/SudokuApplication.java
+++ src/main/java/sudoku/SudokuApplication.java
@@ -1,7 +1,31 @@
 package sudoku;
 
-public class SudokuApplication {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Stack;
+
+public class SudokuApplication extends Application {
+    private IUserInterfaceContract.ViewuiImpl;
+
+
+    public void start(Stage primaryStage) throws Exception{
+        uiImpl = new UserInterfaceImpl(primaryStage);
+        try{
+            SudokuBuildLogic.build(uiImpl);
+        }catch(IOException e){
+            e.printStackTrace();
+            throw e;
+        }
     }
+
+    public static void main(String[] arga){
+        launch(args);
+    }
+
+
 }
